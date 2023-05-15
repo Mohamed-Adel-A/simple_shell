f63a50e57c6260923d7c5b7e67ecf9365e830cfe
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-15 13:07:55 +0300

    Update testing.c

--- testing.c
+++ testing.c
@@ -6,8 +6,8 @@ int main(void)
   char line[] = "This is a test line\n";
   char *new_line = _strdup(line);
 
-	printf("%s, %p\n", line, line);
-	printf("%s, %p\n", new_line, new_line);
+	printf("%s len: %i, %p\n", line, _strlen(line), line);
+	printf("%s len: %i, %p\n", new_line, _strlen(new_line), new_line);
 	
 	return (1);	
 }
