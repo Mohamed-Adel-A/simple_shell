b8dd14842f2245080d44d7ed37c7af1ad4d2e5a4
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-15 14:05:45 +0300

    Update testing.c

--- testing.c
+++ testing.c
@@ -5,7 +5,11 @@ int main(void)
 {
   char line[] = "This is a test line\n";
   char *new_line = _strdup(line);
+	char **tokens = NULL;
+	
+	tokens = tokenizing(line);
 
+	printf("%s len: %i, %p\n", tokens[0], _strlen(tokens[0]), tokens[0]);
 	printf("%s len: %i, %p\n", line, _strlen(line), line);
 	printf("%s len: %i, %p\n", new_line, _strlen(new_line), new_line);
 	
