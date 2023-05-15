a0aa8b3506f3a2542f69a7ec8a9f269e59235914
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-15 14:08:23 +0300

    Update tokenizing.c

--- tokenizing.c
+++ tokenizing.c
@@ -17,32 +17,24 @@ char **tokenizing(char *line)
 	if(line == NULL)
 		return (NULL);
 
-	printf("strat\n");
-	printf("%s\n", line);
 	line_cpy = _strdup(line);
 	/*strcpy(line_c, line);*/
 	token = strtok(line, delim);
-	printf("%s\n", line);
 	while (token != NULL)
 	{
-		token_idx = (token - line);
-		printf("%i : %s (%s)\n", tokens_count, token, line + token_idx);
 		token = strtok(NULL, delim);
 		tokens_count++;		
 	}
 
-	printf("tokens_count : %i", tokens_count);
 	tokens = malloc((tokens_count + 1) * sizeof(char *));
 	if (tokens == NULL)
 		return (NULL);
-	printf("%s\n", line_cpy);
 	token = strtok(line_cpy, delim);
 	tokens_count = 0;
 	while (token)
 	{
 		token_idx = (token - line_cpy);
 		tokens[tokens_count] = line + token_idx;
-		printf("%i : %s (%li)\n", tokens_count, tokens[tokens_count], line_cpy - token);
 		token = strtok(NULL, delim);
 		tokens_count++;
 	}
