feefc4fade8ca4baa738ac59a2db2c85445997fe
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-17 21:56:39 +0300

    Update env_helper.c

--- env_helper.c
+++ env_helper.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
+#include "shell.h"
 
-
-
-char *_getenv(const char *name)
+/**
+ * _getenv - get the value of env valiable
+ * @name: name of enviroment variable to be checked
+ *
+ */
+char *_getenv(const char *variable_name)
 {
-	environ
+	int i = 0;
+	int var_len;
+
+	if (name == NULL)
+		return (NULL);
+
+	var_len = _strlen(variable_name);
+	while (environ[i] != NULL)
+	{
+		if ((_strncmp(variable_name, environ[i], var_len) == 0) &&
+		     (environ[i][var_len] == '=') )
+		{
+			return (environ[i] + var_len + 1);
+		}
+		i++;
+	}
+
+	return (NULL);
 }
