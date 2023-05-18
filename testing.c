87593b1d37aeb5ba9e27eb29111b32c3078cec08
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-18 11:02:09 +0300

    Update testing.c

--- testing.c
+++ testing.c
@@ -31,6 +31,8 @@ int main(void)
 	pid_t cpid;
 	/**char *cmd[] = {"/bin/ls", "-l", NULL};*/
 	printf("Path =%s\n", _getenv("PATH"));
+	_setenv("new", "new value", 0);
+	printf("Path =%s\n", _getenv("new"));
 	while (1)
 	{
 		line = NULL;
