74ad577391a2e75e12b16d5db1f5745f0ba8f7ea
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-18 10:28:00 +0300

    Update env_helper.c

--- env_helper.c
+++ env_helper.c
@@ -53,6 +53,39 @@ void *_memcpy(void *dest, void *src, size_t n)
 	return dest;
 }
 
+/**
+ *  _realloc - reallocate memeory to a new size
+ * @prt: pointer to memory to be allocated
+ * @size: new size
+ *
+ * Return: pointer to new memory, or NULL on error
+ */
+void *_realloc(void *ptr, size_t size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL && size == 0)
+		return (NULL);
+	else if (ptr == NULL && size != 0)
+	{
+		new_ptr = malloc(size);
+		return (new_ptr);
+	}
+	else if (ptr == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	_memcpy(new_ptr, ptr, size);
+	free(ptr);
+
+	return (new_ptr);
+}
 
 /**
  * _setenv - adds variable to the environment
