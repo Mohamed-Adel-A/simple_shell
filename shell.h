a6ec3eeb70228e61804c2bcb8b1131fec5c46770
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-18 10:30:21 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -27,6 +27,10 @@ char *_strdup(const char *src);
 int _strncmp(char *s1, char *s2, size_t n);
 
 
+/* mem_helper.c */
+void *_memcpy(void *dest, void *src, size_t n);
+void *_realloc(void *ptr, size_t size);
+
 /* tokenizing.c */
 char **tokenizing(char *line);
 
