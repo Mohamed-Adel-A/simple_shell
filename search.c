478ba784a5ed2c5d8260e27f9953765ff93a4e3a
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-19 20:37:54 +0300

    Update search.c

--- search.c
+++ search.c
@@ -1,25 +1,6 @@
 #include "shell.h"
 
-/**
- * str_start_with - chaech if string start wiht other string
- * @str: string to be checked
- * @start_str: string to check if str start with it
- *
- * Return: 1 if str start with start_str, 0 otherwise
- */
-int str_start_with(const char *str, const char *start_str)
-{
-	int i = 0;
-	if (str == NULL || start_str == NULL)
-		return (0);
-	for (i = 0; start_str[i] != '\0' ; i++)
-	{
-		if (start_str[i] != str[i])
-			return (0);
-	}
 
-	return (1);
-}
 
 /**
  * extract_path - extract path and filename from full path
@@ -80,6 +61,7 @@ int program_search(const char *fullpath)
 	ret_extract = extract_path(fullpath, path, name);
 	if (ret_extract == 0)
 		return (0);
+
 	
 	
 	
