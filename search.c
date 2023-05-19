c7a0959a7c45313d358a3007f1fb88c96f095901
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-05-19 20:27:32 +0300

    Update search.c

--- search.c
+++ search.c
@@ -21,34 +21,66 @@ int str_start_with(const char *str, const char *start_str)
 	return (1);
 }
 
-char * extract_path(const char *str)
+/**
+ * extract_path - extract path and filename from full path
+ * @fullpath: full path containg path and filename
+ * @path: extracted path
+ * @filename: extracted filename
+ *
+ * Return: 1 if fined path and filename, 0 otherwise
+ */
+int extract_path(const char *str, char *path, char *file_name)
 {
 	int slash_pos = -1, i = 0;
 
-	if (!(str_start_with(str, "./")) || !(str_start_with(str, "/")))
+	if (!(str_start_with(str, "./")) && !(str_start_with(str, "/")))
 	{
-		return (NULL);
+		path[i] = '\0';
+		return (0);
 	}
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[i] = '/')
+		if (str[i] == '/')
 			slash_pos = i;
 	}
 	if (slash_pos == -1)
-		return (NULL)
-	_str
-	
+	{
+		path[0]='\0';
+		return (0);
+	}
+
+	_memcpy(path, str, slash_pos + 1);
+	path[slash_pos + 1]= '\0';
+	if (str[slash_pos + 1] != '\0')
+	{
+		_strcpy(file_name, (str + slash_pos + 1));
+	}
+	else
+	{
+		file_name = NULL;
+		return (0);
+	}
+
+	return (1);
 }
 
 
 
-int program_search(const char *prog)
+int program_search(const char *fullpath)
 {
-	int start;
+	int start, ret_extract;
+	char path[1024], file_name[265];
 
-	if (str_start_with(prog, "./"))
+	if (!str_start_with(fullpath, "./") && !str_start_with(fullpath, "/"))
 	{
-		
+		return (0);
 	}
+
+	ret_extract = extract_path(fullpath, path, name);
+	if (ret_extract == 0)
+		return (0);
+	
+	
+	
 }
