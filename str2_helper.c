2ede4f8f27368b17063de7034ae14f0c9b9b0f97
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-20 22:49:44 +0300

    Create str2_helper.c

---
+++ str2_helper.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * strchar - find first occurrence of a character in string
+ * @str: the string to be searched
+ * @c: the character we want to find
+ *
+ * Return: pointer to the character, NULL if not found
+ */
+char *_strchar(const char *str, char c)
+{
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] == c)
+			return (&str[i]);
+	}
+
+	if (str[i] == c)
+		return (&str[i]);
+
+	return (NULL);
+}
