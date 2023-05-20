fbf0323b881b1ec9634806aa9013d855ba5b94ba
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-20 21:26:45 +0300

    Update testing.c

--- testing.c
+++ testing.c
@@ -49,6 +49,7 @@ int main(int argc , char **argv)
 	printf("Path =%s\n", _getenv("PATH"));
 	_setenv("PATH", "new value", 1);
 	printf("Path =%s\n", _getenv("PATH"));
+	print_env();
 	
 	while (1)
 	{
