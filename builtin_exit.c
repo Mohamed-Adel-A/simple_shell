bfd107b28915d021d9678dd1b2d57eba47d905bc
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-22 00:26:05 +0300

    Update builtin_exit.c

--- builtin_exit.c
+++ builtin_exit.c
@@ -5,20 +5,23 @@ int builtin_exit(char *exit_st_str)
 	/*char *exit_st_str;*/
 	int exit_st_int = 0;
 
-	if (_isnumber(exit_st_str))
+	if (exit_st_str != NULL)
 	{
-		errno = EINVAL;
-		return (-1);
-	}
-	exit_st_int = _atoi(exit_st_str);
-	if (exit_st_int < 0)
-	{
-		errno = EINVAL;
-		return (-1);
-	}
-	else if (exit_st_int > 255)
-	{
-		exit_st_int = exit_st_int % 256;
+		if (_isnumber(exit_st_str))
+		{
+			errno = EINVAL;
+			return (-1);
+		}
+		exit_st_int = _atoi(exit_st_str);
+		if (exit_st_int < 0)
+		{
+			errno = EINVAL;
+			return (-1);
+		}
+		else if (exit_st_int > 255)
+		{
+			exit_st_int = exit_st_int % 256;
+		}
 	}
 
 	/*free_vars();*/
