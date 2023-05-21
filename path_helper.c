0bcc490010b711b829023256f492020a1b3048ee
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-21 15:58:37 +0300

    Update path_helper.c

--- path_helper.c
+++ path_helper.c
@@ -2,14 +2,14 @@
 
 char *check_paths(char *cmd)
 {
-	char *all_paths = _strdup(_getenv("PATH"));
+	char *all_paths;
 	char *paths_array, *single_path;
 	int i = 0, cmd_len, path_len;
 
 	cmd_len = _strlen(cmd);
+	all_paths = _strdup(_getenv("PATH"));
 	paths_array = tokenizing(all_paths, ":");
 
-	
 	for (i = 0 ; paths_array[i] != NULL ; i++)
 	{
 		path_len = _strlen(paths_array[i]);
