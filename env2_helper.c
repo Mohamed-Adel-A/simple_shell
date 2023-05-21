dab4d07e9448f0d4ff079f1e57a2dd59b57b9d0c
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-22 02:41:55 +0300

    Update env2_helper.c

--- env2_helper.c
+++ env2_helper.c
@@ -1,9 +1,25 @@
 #include "shell.h"
 
 /*****************************************/
+int builtin_env(char **args);
 void print_env(void);
 char *_getenv(const char *variable_name);
 /*****************************************/
+
+
+int builtin_setenv(char **args)
+{
+	int setenv_ret;
+
+	setenv_ret = _setenv(args[1], args[2], 1);
+	if (setenv_ret == -1)
+	{
+		return (-1);
+	}
+
+	return (0);
+}
+
 
 /**
  * buitin_en - the fuction associated with env cmd
@@ -11,9 +27,9 @@ char *_getenv(const char *variable_name);
  *
  * Return: 0 if success, -1 if failed
  */
-int builtin_env(char *arg)
+int builtin_env(char **args)
 {
-	if (arg != NULL)
+	if (args[1] != NULL)
 	{
 		errno = EINVAL;
 		return (-1);
