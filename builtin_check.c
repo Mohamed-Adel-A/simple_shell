6aeb10757187805ff7d5b2347b8a8fb39da48fd7
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-21 20:44:17 +0300

    Create builtin_check.c

---
+++ builtin_check.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * struct buitins - struct contains builins names and fuctions
+ * @name: string contains the command to be used to call the function
+ * @funcction: pointer to the buitin function to be called
+ *
+ */
+typedef struct buitins
+{
+	char *name;
+	int (*function)(char *arg); 
+} buitin_t;
+
+/**
+ * check_builtin - check if the command is buitlin command
+ * @cmd: the command to be checked
+ *
+ * Return: pointer to the function to be called of NUll if not
+ */
+int check_builtin(char *cmd)
+{
+	int i = 0, cmd_len;
+	buitin_t builtins_array[] = { {"exit", exit_builtin},
+							   {"env", env_builtin},
+							   {"cd", cd_builtin},
+							   {NULL, NULL}
+							  };
+	if (cmd == NULL)
+		return (NULL);
+
+	cmd_len = _strlen(cmd);
+	for (i = 0; builtins_array[i].name != NULL; i++)
+	{
+		if (_strncmp(builtins_array.name, cmd, cmd_len + 1) == 0)
+		{
+			return (builtins_array[i].function);
+		}
+	}
+
+	return (NULL);			
+}
