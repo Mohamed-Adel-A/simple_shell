1cc8af589b6d31c3079792a309a741fb4abba35d
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-21 15:39:28 +0300

    Update PATH_helper.c

--- PATH_helper.c
+++ PATH_helper.c
@@ -3,10 +3,18 @@
 char *check_paths(char *cmd)
 {
 	char *all_paths = _strdup(_getenv("PATH"));
-	char *single_parh;
+	char *paths_array, *single_path;
+	int i = 0, cmd_len, path_len;
+
+	cmd_len = _strlen(cmd);
+	paths_array = tokenizing(all_paths, ":");
 
 	
-
-	
+	for (i = 0 ; paths_array[i] != NULL ; i++)
+	{
+		single_path = malloc(sizeof(char) * (_strlen(paths_array[i]) + _strlen(cmd) + 1));
+		_memcpy(single_path, _strlen(paths_array[i], );
+		printf("&s\n", paths_array[i]);
+	}
 	
 }
