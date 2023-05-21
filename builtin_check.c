abf3175ca7e995d1cf89c98ae0f9d4a4431affde
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-22 00:08:45 +0300

    Update builtin_check.c

--- builtin_check.c
+++ builtin_check.c
@@ -29,11 +29,11 @@ int (*check_builtin(char *cmd))(char *)
 		return (NULL);
 
 	cmd_len = _strlen(cmd);
-	for (i = 0; builtins_array[i]->name != NULL; i++)
+	for (i = 0; builtins_array[i].name != NULL; i++)
 	{
-		if (_strncmp(builtins_array.name, cmd, cmd_len + 1) == 0)
+		if (_strncmp(builtins_array[i].name, cmd, cmd_len + 1) == 0)
 		{
-			return (builtins_array[i]->function);
+			return (builtins_array[i].function);
 		}
 	}
 
