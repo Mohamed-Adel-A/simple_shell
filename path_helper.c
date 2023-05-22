8929e4aacf5cc7870e00a6dda82fe8c6f0b97e73
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-22 03:22:23 +0300

    Update path_helper.c

--- path_helper.c
+++ path_helper.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * check_path - search for cmd in paths in PATH
+ * @cmd: the program name
+ *
+ * Return: pointer to full path of the program, NULL if not found
+ */
 char *check_paths(char *cmd)
 {
 	char *all_paths, *single_path = NULL;
@@ -10,13 +16,12 @@ char *check_paths(char *cmd)
 	all_paths = _strdup(_getenv("PATH"));
 	paths_array = tokenizing(all_paths, ":");
 
-	
 	for (i = 0 ; paths_array[i] != NULL ; i++)
 	{
 		path_len = _strlen(paths_array[i]);
 		single_path = malloc(sizeof(char) * (path_len + cmd_len + 2));
 		_memcpy(single_path, paths_array[i], path_len);
-		single_path[path_len] = '/'; 
+		single_path[path_len] = '/';
 		_memcpy(single_path + path_len + 1, cmd, cmd_len);
 		single_path[path_len + cmd_len + 1] = '\0';
 		if (access(single_path, F_OK) == 0)
@@ -27,11 +32,9 @@ char *check_paths(char *cmd)
 		free(single_path);
 		single_path = NULL;
 	}
-	
 
 	free(all_paths);
 	free(paths_array);
 
-	return (single_path);	
+	return (single_path);
 }
-
