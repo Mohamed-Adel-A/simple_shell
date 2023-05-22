a3302ab77ebab847e69ea546d4e79ba042159b89
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-22 03:59:29 +0300

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * handle_comments - handle comments in entered line
+ * @line: pointer to string line
+ *
+ * Return: 0 in success, -1 otherwise
+ */
+int handle_comments(char *line)
+{
+	int i;
+
+	if (line == NULL)
+		return (-1);
+
+	for (i = 0 ; line[i] != '#' ; i++ )
+	{
+	}
+	line[i] = '\0';
+
+	return (0);
+}
