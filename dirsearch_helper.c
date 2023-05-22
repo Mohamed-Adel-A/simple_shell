527c213df0e7d3794796322aff18da1c26b20d3c
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-22 03:15:32 +0300

    Update dirsearch_helper.c

--- dirsearch_helper.c
+++ dirsearch_helper.c
@@ -35,7 +35,7 @@ int extract_path(const char *fullpath, char *path, char *file_name)
 	int slash_pos = -1, i = 0;
 
 	if (!(str_start_with(fullpath, "./")) && !(str_start_with(fullpath, "/")) &&
-	    !(str_start_with(fullpath, "../")) )
+	    !(str_start_with(fullpath, "../")))
 	{
 		path[i] = '\0';
 		return (0);
