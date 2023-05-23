f7b4c24dcc6b997b6ba117bc2ffdb1159893ef65
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-24 02:16:56 +0300

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -71,3 +71,59 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	*lineptr = lline;
 	return (i);
 }
+
+
+/**
+ * check_empty_line - check if line is empty
+ * @line: entered line
+ *
+ * Return: 1 in success, 0 in failure
+ */
+ int check_empty_line(const char *line)
+ {
+	 int i = 0;
+	 while (line[i] != '\0')
+	 {
+		 if (line[i] != ' ' && line[i] != '\t' && line[i] != '\n')
+			 return (0);
+		 i++;
+	 }
+	 return (1);
+ }
+
+
+
+/**
+ * getting_line - get line via getline and then check the line
+ * @sh_data: shell data
+ *
+ * Return: 0 in success, -1 in failure
+ */
+int getting_line(shell_data_t *sh_data)
+{
+	ssize_t getline_ret;
+	size_t line_len = 0;
+	char *line = NULL;
+
+	sh_data->line = NULL;
+	sh_data->tokens = NULL;
+	sh_data->cmd_path = NULL;
+
+	getline_ret = _getline(&line, &line_len, stdin);
+	if (getline_ret == -1)
+	{
+		free_all(sh_data);
+		_puts("\n");
+		exit(EXIT_SUCCESS);
+	}
+	else if (check_empty_line(line))
+	{
+		free(line);
+		return (-1);
+	}
+
+	handle_comments(line);
+	sh_data->line = line;
+	sh_data->tokens = tokenizing(line, " \n");
+	return (0);
+}
