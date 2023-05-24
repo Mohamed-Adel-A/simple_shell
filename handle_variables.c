2af4664a2dbcf2a95eae591ce8e76044ead8af9b
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-24 22:00:51 +0300

    Update handle_variables.c

--- handle_variables.c
+++ handle_variables.c
@@ -60,7 +60,7 @@ int add_variable(char *var, shell_data_t *sh_data)
 		{
 		}
 
-		new_vars = _realloc(old_vars, sizeof(char *) * (i + 3));
+		new_vars = _realloc(old_vars, sizeof(char *) * (i + 2));
 		if (new_vars == NULL)
 		{
 			return (-1);
