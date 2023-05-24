66c468af57d9a165c0fb7972ccb0719535b81b74
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-24 04:00:12 +0300

    Update utilities.c

--- utilities.c
+++ utilities.c
@@ -6,7 +6,7 @@ void handle_signal(int signal_no)
 	if (signal_no == SIGINT)
 	{
 		/*fflush(stdin);*/
-		write(STDIN_FILENO, "$) \n", 3);
+		write(STDIN_FILENO, "\n$) ", 4);
 		/*puts("\n");
 		prompt();*/
 	}
