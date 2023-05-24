e739c4111323f481ad1ddd11657e6362a954ab9f
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-24 18:57:47 +0300

    Update handle_variables.c

--- handle_variables.c
+++ handle_variables.c
@@ -90,10 +90,10 @@ int check_variable(int token_idx, shell_data_t *sh_data)
 	memcpy(full_str, str, var_pos);
 	full_str[var_pos] = '\0';
 	printf("%s\n", full_str);
-	printf("%s\n", var_str);
+	printf("++%s\n", var_str);
 	memcpy(full_str + var_pos, var_str, var_len);
 	full_str[var_pos + var_pos + var_len] = '\0';
-	printf("%s\n", full_str);
+	printf("--%s\n", full_str);
 	strcpy(full_str + var_pos + var_len, str + i);
 
 	sh_data->tokens[token_idx] = full_str;
