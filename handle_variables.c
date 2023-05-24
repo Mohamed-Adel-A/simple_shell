fe9957052562db043ebf1723b2ac75ac5e5c2665
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-24 19:15:33 +0300

    Update handle_variables.c

--- handle_variables.c
+++ handle_variables.c
@@ -37,7 +37,7 @@ int check_variable(int token_idx, shell_data_t *sh_data)
 
 	while (str[i] != '\0')
 	{
-		var_pos = _strchar(str, '$');
+		var_pos = _strchar(str + i, '$');
 		if (var_pos == -1)
 			return (0);
 		i = var_pos + 1;
