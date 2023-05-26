feed0686c855176e9a7b3d7ee9870a22e148600f
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-26 06:00:57 +0300

    Update handle_semicolons.c

--- handle_semicolons.c
+++ handle_semicolons.c
@@ -19,7 +19,7 @@ int handle_semicolons(shell_data_t *sh_data)
 	}
 	colon_pos = i;
 	diff_pos = i - (sh_data->next_tokens_index);
-	if ((diff_pos == 0 || diff_pos == 1)  && tokens[i] != NULL)
+	if (diff_pos == 0  && tokens[i] != NULL)
 	{
 		printf("i= %i, index= %i, diff= %i", i, sh_data->next_tokens_index, diff_pos);
 		sh_data->next_tokens_index++;
