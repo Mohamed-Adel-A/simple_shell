95cf2e139c9cd8eed8f3d7febe2699d9851ad695
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-26 17:14:07 +0300

    Update _getline.c

--- _getline.c
+++ _getline.c
@@ -140,7 +140,7 @@ int getting_line(shell_data_t *sh_data)
 	size_t line_len = 0;
 	char *line = NULL;
 
-	getline_ret = _getline(&line, &line_len, stdin);
+	getline_ret = _getline(&line, &line_len, sh_data->fd);
 
 	if (getline_ret == -1)
 	{
