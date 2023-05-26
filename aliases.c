9e6e5791f7c79e777c3e6ff2929fbbbf840593df
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-05-27 02:16:59 +0300

    Update aliases.c

--- aliases.c
+++ aliases.c
@@ -1,9 +1,21 @@
 #include "shell.h"
 
 
-builtin_alias()
+builtin_alias(shell_data_t *sh_data)
 {
-	
+	int i;
+	char *tokens = sh_data->tokens;
+
+	if (tokens[1] == NULL)
+	{
+		print_aliases(sh_data);
+		return (0);
+	}
+
+	for(i = 0; tokens[i] != NULL; i++)
+	{
+		
+	}
 }
 
 
@@ -11,8 +23,17 @@ builtin_alias()
 void print_aliases(shell_data_t *sh_data)
 {
 	int i;
+	aliases_t *aliases = sh_data->aliases;
 
-	if (sh_data->aliases == NULL)
+	if (aliases == NULL)
+		return;
+	for (i = 0 ; aliases[i] != NULL ; i++)
 	{
+		_puts(aliases[i][name]);
+		_puts("=");
+		_puts("\'");
+		_puts(aliases[i][value]);
+		_puts("\'");
+		_puts("\n");
 	}
 }
